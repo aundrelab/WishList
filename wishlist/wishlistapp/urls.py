7b255e84b41b0ebb3c5ec8dc7dcf48b3133a838a
Prince Rios <riosprince7@gmail.com>
2021-09-27 23:52:59 -0700

    Created users model to be stored in database. Created REST Api to display users as json using the users endpoint.

--- wishlist/wishlistapp/urls.py
+++ wishlist/wishlistapp/urls.py
@@ -1,7 +1,11 @@
 from . import views
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+from wishlistapp import views
+
 urlpatterns = [
     path('', views.home, name='app-home'),
     path('about/', views.about, name='app-about'),
+    path('users/', views.userList.as_view()),
 ]
--- wishlist/wishlistapp/views.py
+++ wishlist/wishlistapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import users
+from . serializers import userSerializer
 
 # Create your views here.
 def home(request):
@@ -7,3 +13,9 @@ def home(request):
 
 def about(request):
     return HttpResponse('<h1>The about page</h1>')
+
+class userList(APIView):
+    def get(self, request):
+        users1 = users.objects.all()
+        serializer = userSerializer(users1, many=True)
+        return Response(serializer.data)
