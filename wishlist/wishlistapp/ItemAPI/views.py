acee4e2ce98e71d12b42a91e346fd24605a115be
Alex <alexespinoza11f@gmail.com>
2021-09-29 22:00:22 -0700

    Created update item rest api.

--- wishlist/wishlistapp/ItemAPI/views.py
+++ wishlist/wishlistapp/ItemAPI/views.py
@@ -1,10 +1,17 @@
 from ..models import Item
+from django.shortcuts import redirect
 
 def update_item(item_id, title, description, category, image_url, item_url):
-    item = Item.objects.get(item_id)
+    item = Item.objects.get(id=item_id)
     item.title = title;
     item.description = description
     item.category = category
     item.imageURL = image_url
     item.itemURL = item_url
     item.save()
+    return redirect("url_name")
+
+def delete_item(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    item.delete()
+    return redirect("list-items")
--- wishlist/wishlistapp/urls.py
+++ wishlist/wishlistapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .ItemAPI import views
 
 urlpatterns = [
     path('', views.home, name='app-home'),
     path('about/', views.about, name='app-about'),
+    path('delete/<item_id>', views.delete_item, name="delete-item"),
+    path('update/<item_id>', views.update_item, name='update-item'),
 ]
