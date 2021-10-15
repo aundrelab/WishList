049ab925f9f8d2173fa2bf09eee2d368a4de6f08
Alex <alexespinoza11f@gmail.com>
2021-10-15 16:17:32 -0700

    updates settings\

--- wishlist/wishlistapp/urls.py
+++ wishlist/wishlistapp/urls.py
@@ -4,7 +4,7 @@ from .ItemAPI import views as item_views
 from .ListAPI import views as list_views
 
 urlpatterns = [
-    path('home', views.home, name='app-home'),
+    path('', views.home, name='app-home'),
     path('login', views.login, name='app-login'),
     path('signup', views.signup, name='app-signup'),
     path('newItem', views.newItem, name='app-newItem'),
